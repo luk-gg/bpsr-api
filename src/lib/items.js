2251576aafc1dddce07d92ec7117d4e5324edfcb
Zakum <mirveo@gmail.com>
2025-09-21 21:32:56 -0700

    remove recursive recipe creation in items.js

--- src/lib/items.js
+++ src/lib/items.js
@@ -1,6 +1,4 @@
 import { completeCommonData, getBriefArr } from "./utils"
-// import usedInMap from "./item_used_in"
-import text_en from "$client/Lang/english.json";
 import ItemTable from "$client/Tables/ItemTable.json";
 import ItemTypeTable from "$client/Tables/ItemTypeTable.json";
 import StallDetailTable from "$client/Tables/StallDetailTable.json";
@@ -8,56 +6,20 @@ import sourcesMap from "./item_sources";
 import usedInMap from "./item_used_in";
 
 // Adds recipes at a depth of 1
-const itemsBase = Object.values(ItemTable).map(item => {
+const entries = Object.values(ItemTable).map(item => {
     const usedIn = usedInMap[item.Id] ?? []
     const sources = sourcesMap[item.Id]
-    const sellable = Boolean(StallDetailTable[item.Id])
     const sellData = StallDetailTable[item.Id] || null
+    const sellable = Boolean(sellData)
 
     return {
         ...item,
         ...completeCommonData(item),
-        // sellable,
-        // storable?,
-        // sellData,
+        sellable,
+        sellData,
         usedIn,
         sources,
-    }
-})
-
-// Turn array of items into an object for easy lookup
-const itemsBaseMap = itemsBase.reduce((acc, curr) => {
-    acc[curr.Id] = curr
-    return acc
-}, {})
-
-function walkRecipeTree(recipe) {
-    if (recipe.NeedMaterial.length < 1) return recipe
-
-    const newMaterials = recipe.NeedMaterial
-        .filter(([itemId]) => itemId !== 0)
-        .map(([itemId, amount]) => {
-            return [itemsBaseMap[itemId], amount]
-        })
-        // Item id 303 causes recipe not to be found in ProdTable 2010031 and 2010214 (food items)
-        .map(([item, amount]) => {
-            // if (!item) console.log(recipe.Id)
-            return [{ ...item, recipes: item?.recipes.map(recipe => walkRecipeTree(recipe)) }, amount]
-        })
-
-    return {
-        ...recipe,
-        NeedMaterial: newMaterials
-    }
-}
-
-// Recursively iterates recipes to replace material IDs with item objects
-const entries = itemsBase.map((item) => {
-    return item
-    const recipes = item.recipes.map(recipe => walkRecipeTree(recipe))
-    return {
-        ...item,
-        recipes
+        // storable?,
     }
 })
 
