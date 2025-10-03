7193ddc83e3f7a86453ad39cc5b0eb57ec1a1cfc
Zakum <mirveo@gmail.com>
2025-10-03 02:01:10 -0700

    life skill recipe trees finished refactoring

---
+++ src/lib/life_skill_recipe_trees.js
@@ -0,0 +1,111 @@
+import life_skill_recipes, { getMaterials } from "./life_skill_recipes";
+import { getBriefItem } from "./utils";
+
+// TODO: add back certain things like talent yields, required talents to obtain, etc.
+// TODO: make variable materials have a list of "options"
+
+// A recipe only has one RelatedItemId despite Award being an array.
+// An item can have multiple recipes (Mystery Metal) however material items that are crafted seemingly only have one recipe.
+
+// Current code does not handle "OR", where a material can have multiple recipes.
+// Sources are omitted because it makes the file go from 3 MB to fucking 498 MB.
+
+const recipesArr = Object.values(life_skill_recipes)
+
+function getChildren(prevDepth, depth) {
+    const matsWithRecipes = prevDepth
+        .flatMap((parentMat) => {
+            if (!parentMat.recipe) return []
+
+            const materials = getMaterials(parentMat.recipe).materials
+
+            return materials.map((mat) => {
+                const recipe = recipesArr.find(({ RelatedItemId }) => RelatedItemId === mat.Id) ?? null
+                return {
+                    parent: parentMat.node,
+                    ...mat,
+                    recipe: trimRecipe(recipe)
+                }
+            })
+        })
+
+    return addNodeData(matsWithRecipes, depth)
+}
+
+function addNodeData(materials, depth) {
+    return materials.map((mat, index) => {
+        let node = `${depth}${String.fromCharCode(97 + index)}`
+        let edge = mat.parent ? `${mat.parent}-${node}` : null
+        return {
+            node,
+            edge,
+            ...mat,
+        }
+    })
+}
+
+function trimRecipe(recipe) {
+    if (!recipe) return null
+
+    const {
+        Id,
+        RelatedItemId,
+        Cost,
+        Time,
+        NeedMaterial,
+        NeedMaterialType,
+        UnlockCondition,
+        LifeProId,
+        Exp,
+        SpecialAward,
+        talent_lv0_yields,
+        talent_lv1_yields,
+        talent_lv2_yields,
+        talent_lv3_yields
+    } = recipe
+
+    return {
+        Id,
+        RelatedItemId,
+        Cost,
+        Time,
+        NeedMaterial,
+        NeedMaterialType,
+        UnlockCondition,
+        LifeProId,
+        Exp,
+        SpecialAward,
+        talent_lv0_yields,
+        talent_lv1_yields,
+        talent_lv2_yields,
+        talent_lv3_yields
+    }
+}
+
+function getCraftingTreeForRecipe(rootRecipe) {
+    let depth = 0;
+
+    const result = [
+        addNodeData([{ parent: null, ...getBriefItem(rootRecipe.RelatedItemId), recipe: trimRecipe(rootRecipe) }], depth)
+    ];
+
+    while (true) {
+        const prevDepth = result[depth++]
+        const currDepth = getChildren(prevDepth, depth);
+        if (currDepth.length < 1) break
+        result.push(currDepth)
+    }
+
+    return result.flat()
+}
+
+const recipesWithMaterialTrees = Object.values(life_skill_recipes)
+    // .filter(recipe => recipe.Id === 2020027)
+    .reduce((acc, recipe) => {
+        acc[recipe.Id] = getCraftingTreeForRecipe(recipe)
+        return acc
+    }, {})
+
+export const entries_brief = []
+
+export default recipesWithMaterialTrees
